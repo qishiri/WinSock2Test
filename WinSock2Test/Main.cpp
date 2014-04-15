96be9f72ed47fd347ac1ce7dfc244c94c95a9a58
qishiri <170cm.kurekure@gmail.com>
2014-04-16 01:51:03 +0900

    ベータ版

--- WinSock2Test/Main.cpp
+++ WinSock2Test/Main.cpp
@@ -14,11 +14,10 @@ int main(void)
 	std::string tag = "Roosan";
 	std::string tagsearchURL = "www.nicovideo.jp/tag/" + UrlEncode(SJISToUTF8(tag.c_str())) + "?rss=2.0&sort=v";
 	RequestString thread_id;
-	//RequestString thread_id("flapi.nicovideo.jp/api/getflv?v=sm9");
 	ConnectSocket cs;
 	NicoNicoTools nt;
 
-	std::string user = "oe9RXwaDOf_tN5Fam55aYJuMK_U";
+	std::string user = "QBkpRLSYQYrVGUeAu8YAIuEa1sE";
 	std::vector<std::string> commentlist, movieidlist;
 	std::map<string, vector<string>> allcommentlist;
 
